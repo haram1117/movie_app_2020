a5e954eaa04edd01ccc5cf625722aab54b2acaa9
Haram <haram1117@khu.ac.kr>
2020-07-04 10:48:29 +0900

    #3-2. State: Planning the Movie Component

--- src/App.js
+++ src/App.js
@@ -2,27 +2,20 @@ import React from 'react';
 
 class App extends React.Component{
   state={
-    count: 0
-  };
-  add=()=>{
-    this.setState(current =>({count: current.count++}));
-  };
-  minus=()=>{
-    this.setState(current =>({count: current.count--}));//setState에서 this.state등을 하는 것은 좋지 않음 current쓰는 방식 알기, setState할때마다 render()함수가 다시 실행
+    isLoading: true,
+    movies : []
   };
   componentDidMount(){
-    console.log("Component rendered");
+    setTimeout(()=>{
+      this.setState({isLoading: false});
+    }, 6000)
   }
-  componentDidUpdate(){
-    console.log("Component Updated");
-  }
+  
   render(){
-    console.log("I'm rendering");
-    return (<div>
-      <h1>The number is: {this.state.count}</h1>
-      <button onClick={this.add}>Add</button>
-      <button onClick={this.minus}>Minus</button>
-      </div>)
+    const {isLoading} = this.state;
+    return (
+    <div>{isLoading ? "Loading..." : "We are ready"}</div>
+    )
   }
 
 }
